1ca7194244a8baf851a8cff2302440ca3c027a28
Anant Girdhar <anant.girdhar@proton.me>
2023-12-02 00:56:36 -0500

    Update template

--- 2023/template.py
+++ 2023/template.py
@@ -1,12 +1,21 @@
-"""Day <++>
-"""
+"""Day <++>"""
 
 import sys
 
-def main(filename):
+def parse_data(data):
+    """Parse the data to make it usable"""
+    <++>
+
+def read_data(filename):
+    """Read the data from the file"""
     with open(filename, 'r') as infile:
         data = infile.read()
     data = data.splitlines()
+    return data
+
+def main(filename):
+    data = read_data(filename)
+    data = parse_data(data)
     <++>
     return data
 
