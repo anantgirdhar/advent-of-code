3ce26e2a8be35fed7f6b4f34f4c90d8fc3e0796c
Anant Girdhar <anant.girdhar@proton.me>
2023-12-01 20:32:23 -0500

    Add a template for the 2023 solutions

---
+++ 2023/template.py
@@ -0,0 +1,15 @@
+"""Day <++>
+"""
+
+import sys
+
+def main(filename):
+    with open(filename, 'r') as infile:
+        data = infile.read()
+    data = data.splitlines()
+    <++>
+    return data
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    <++> = main(filename)
