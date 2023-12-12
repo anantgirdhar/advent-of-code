21f9d86c8efe6e26bf89d346df6768fc84ed71a9
Anant Girdhar <anant.girdhar@proton.me>
2023-12-12 00:36:35 -0500

    Update day 11 part 1
    Instead of expanding the image representing the universe and then
    locating the galaxies, this commit updates the code so that it can
    update the locations of the galaxies directly based on the old image.
    This will hopefully make it easier to extend to part 2.

--- 2023/day11.py
+++ 2023/day11.py
@@ -15,6 +15,36 @@ def compute_total_pairwise_distances(galaxy_locations):
             distances.append(abs(g1x-g2x) + abs(g1y-g2y))
     return sum(distances)
 
+def expand_galaxy_locations(galaxy_locations, num_rows, num_cols):
+    """Return expanded galactic locations
+
+    This function accepts the locations of the galaxies and returns their new
+    locations after the universe has expanded.
+    """
+    # First find the rows and columns that don't have galaxies
+    # These can be identified as the row and column numbers that do not appear
+    # in any of the galaxy locations
+    rows_without_galaxies = list(range(num_rows))
+    columns_without_galaxies = list(range(num_cols))
+    galaxy_rows, galaxy_columns = zip(*galaxy_locations)
+    # Remove all the galaxy rows and columns now
+    for row in set(galaxy_rows):
+        rows_without_galaxies.remove(row)
+    for column in set(galaxy_columns):
+        columns_without_galaxies.remove(column)
+    # Now when we expand these rows and columns, we can compute the new
+    # galactic locations
+    # In this case, we find how many rows and columns come "before" this galaxy
+    # in the universe. Each of those needs to get expanded so the galaxy shifts
+    # by one unit in each direction per additional row and column added
+    new_locations = []
+    for row, column in galaxy_locations:
+        # Figure out how many rows and columns appear before it
+        add_rows = len([r for r in rows_without_galaxies if r < row])
+        add_columns = len([c for c in columns_without_galaxies if c < column])
+        new_locations.append((row + add_rows, column + add_columns))
+    return new_locations
+
 def locate_galaxies(image):
     """Return a list of locations of the galaxies
 
@@ -28,47 +58,6 @@ def locate_galaxies(image):
                 locations.append((i, j))
     return locations
 
-def expand_universe(image):
-    """Return the expanded universe
-
-    This function accepts the image of the galaxies and returns the expanded
-    universe. The rows and columns of the image that don't contain any galaxies
-    expand and become double their width.
-    """
-    # First find the rows and columns that don't have any galaxies
-    # Initialize: Assume that no row or column contains a galaxy and update
-    # this guess whenever one is found
-    rows_without_galaxies = list(range(len(image)))
-    columns_without_galaxies = list(range(len(image[0])))
-    for i, row in enumerate(image):
-        for j, character in enumerate(row):
-            if character == '#':
-                # We found a galaxy in this row and column
-                try:
-                    columns_without_galaxies.remove(j)
-                except ValueError:
-                    # Already removed so don't need to do anything
-                    pass
-                try:
-                    rows_without_galaxies.remove(i)
-                except ValueError:
-                    # Already removed so don't need to do anything
-                    pass
-    expanded_image = []
-    # First expand the columns
-    for row in image:
-        row = list(row)
-        # Make sure to reverse the list of column indices so that, as the
-        # universe expands, we don't need to keep track of new column numbers
-        for j in reversed(columns_without_galaxies):
-            row.insert(j, '.')
-        expanded_image.append(''.join(row))
-    # Now expand the rows by adding rows at the appropriate indices
-    empty_row = '.' * len(expanded_image[0])
-    for i in reversed(rows_without_galaxies):
-        expanded_image.insert(i, empty_row)
-    return expanded_image
-
 def read_data(filename):
     """Read the data from the file"""
     with open(filename, 'r') as infile:
@@ -78,8 +67,12 @@ def read_data(filename):
 
 def main(filename):
     image = read_data(filename)
-    image = expand_universe(image)
     galaxy_locations = locate_galaxies(image)
+    galaxy_locations = expand_galaxy_locations(
+            locate_galaxies(image),
+            num_rows=len(image),
+            num_cols=len(image[0]),
+            )
     total_distance = compute_total_pairwise_distances(galaxy_locations)
     print(f'Total of all pairwise distances: {total_distance}')
     return image
