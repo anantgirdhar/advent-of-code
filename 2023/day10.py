497b29d69505bd6353e5e5fa57c0b9f52eefabd4
Anant Girdhar <anant.girdhar@proton.me>
2023-12-11 20:34:38 -0500

    Add day 10 part 2

--- 2023/day10.py
+++ 2023/day10.py
@@ -49,7 +49,60 @@ def advance(maze, point, direction):
     next_direction = pipe_direction
     return next_point, next_direction
 
-def find_furthest_point(maze, starting_location, connected_pipes):
+def find_enclosed_area(maze, loop):
+    """Find the area enclosed by the loop
+
+    This function accepts the maze as a list of strings, and the loop as a list
+    of points. It then returns the number of points inside the loop as an
+    integer.
+    """
+    # To do this, we just loop over all the points in the maze
+    # The parity (whether a point is inside or outside) changes based on the
+    # following rules. Only points on the loop can cause a change in parity.
+    # |--------------------+---------------|
+    # | Point              | Parity Change |
+    # |--------------------+---------------|
+    # | |                  | Yes           |
+    # |--------------------+---------------|
+    # | -                  | No            |
+    # |--------------------+---------------|
+    # | S-turns (L7 or FJ) | Yes           |
+    # |--------------------+---------------|
+    # | U-turns (F7 or LJ) | No            |
+    # |--------------------+---------------|
+    # Note that these only apply when traversing the maze's points from left to
+    # right
+    parity = 0  # Use 0 to represent outside and 1 for inside
+    area = 0
+    prev_turn = None  # To store the last loop turn
+    for i, line in enumerate(maze):
+        for j, point in enumerate(line):
+            if (i, j) not in loop:
+                # Parity does not change
+                area += parity
+            elif point == '|':
+                # We're going orthogonal to the loop
+                # So the parity changes
+                parity = 1 - parity
+            elif point == '-':
+                # We're moving along the loop
+                # Parity does not change
+                pass
+            elif point in ['L', 'F'] and prev_turn is None:
+                # This is the start of a turn so save it
+                prev_turn = point
+            elif (prev_turn, point) in [('L', '7'), ('F', 'J')]:
+                # This is an S-turn so flip parity and reset the turn
+                parity = 1 - parity
+                prev_turn = None
+            elif (prev_turn, point) in [('F', '7'), ('L', 'J')]:
+                # This is a U-turn so don't flip parity but reset the turn
+                prev_turn = None
+            else:
+                raise ValueError(f'Something weird happened at point {point} ({i}, {j})')
+    return area
+
+def find_furthest_point(maze, starting_location, connected_pipes, return_loop=False):
     """Find the furthest point along the maze
 
     This function accepts the maze as a list of strings, the starting location,
@@ -58,7 +111,12 @@ def find_furthest_point(maze, starting_location, connected_pipes):
     from it and returns the coordinates of the point along with the number of
     steps it would take to get there. This function assumes that there is a
     unique middle point along the loop.
+
+    It can also optionally return the coordinates of the loop
     """
+    if return_loop:
+        loop_coordinates1 = []
+        loop_coordinates2 = []
     # Travel the loop from both sides until you hit the same point
     # Then that is the middle point and is the furthest away
     # We know we're going to move at least one space so advance the initial
@@ -70,10 +128,26 @@ def find_furthest_point(maze, starting_location, connected_pipes):
     # it - so that the points would not be the same at the beginning and we'd
     # enter the loop)
     while point1 != point2:
+        if return_loop:
+            loop_coordinates1.append(point1)
+            loop_coordinates2.append(point2)
         point1, direction1 = advance(maze, point1, direction1)
         point2, direction2 = advance(maze, point2, direction2)
         num_steps += 1
-    return point1, num_steps
+    if return_loop:
+        # Now construct the full list of loop coordinates
+        # Start with the starting location
+        loop_coordinates = [starting_location, ]
+        # Pick a direction and add the points seen in that direction
+        loop_coordinates.extend(loop_coordinates1)
+        # Then we get to the midpoint so add that
+        loop_coordinates.append(point1)
+        # Finally add the rest of the points seen in the other direction
+        # reversed
+        loop_coordinates.extend(reversed(loop_coordinates2))
+        return point1, num_steps, loop_coordinates
+    else:
+        return point1, num_steps
 
 def parse_data(data):
     """Parse the data to make it usable"""
@@ -83,6 +157,8 @@ def parse_data(data):
     # the string
     # I will use this function to find the starting position and the directions
     # of the two pipes connected to the starting position though
+    # Also, use this function to replace the start point pipe 'S' with its
+    # actual shape
     for i, line in enumerate(data):
         if (j := line.find('S')) != -1:
             # The starting position is (i, j)
@@ -114,7 +190,11 @@ def parse_data(data):
         pipe = data[i][j+1]
         if 'W' in PIPES[pipe]:
             connected_pipes.append('E')
-    return start, connected_pipes
+    # Now replace the starting point with its correct pipe shape
+    for pipe, openings in PIPES.items():
+        if connected_pipes[0] in openings and connected_pipes[1] in openings:
+            data[i] = data[i].replace('S', pipe)
+    return start, connected_pipes, data
 
 def read_data(filename):
     """Read the data from the file"""
@@ -125,9 +205,14 @@ def read_data(filename):
 
 def main(filename):
     data = read_data(filename)
-    starting_location, connected_pipes = parse_data(data)
-    furthest_point, num_steps = find_furthest_point(data, starting_location, connected_pipes)
+    starting_location, connected_pipes, data = parse_data(data)
+    furthest_point, num_steps, loop = find_furthest_point(data,
+                                                          starting_location,
+                                                          connected_pipes,
+                                                          return_loop=True)
     print(f'Number of steps to furthest point: {num_steps}')
+    area = find_enclosed_area(data, loop)
+    print(f'Enclosed area: {area}')
     return data, starting_location, connected_pipes
 
 if __name__ == "__main__":
