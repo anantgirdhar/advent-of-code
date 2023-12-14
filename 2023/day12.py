f77f618582bb824b431c5cc4a45c8a85e572bd54
Anant Girdhar <anant.girdhar@proton.me>
2023-12-13 19:04:42 -0500

    Update day 12 part 1
    This commit adds another approach to the problem. It is a string based
    recursive method (and was actually my first attempt at solving the
    problem). I scrapped it and wrote the binary version because I wasn't
    getting the correct answer. Turns out that I had made a mistake
    elsewhere (note to self: if you choose a dict to store data, you are
    assuming that the keys will be unique). Anyway, this commit just adds my
    first recursive approach.

--- 2023/day12.py
+++ 2023/day12.py
@@ -3,9 +3,156 @@
 import itertools as it
 import re
 import sys
-import pdb
 
-def count_permutations(sequence, damage_counts):
+def count_permutations_string_based(sequence, damage_counts):
+    """Count the permutations for a sequence
+
+    This function takes in a sequence string and the counts of damaged springs
+    in the sequence as a tuple. It then returns the number of arrangements of
+    damaged springs that are possible.
+    """
+    counts = 0
+    i = 0
+    while i < len(sequence):
+        # Get the count of damaged springs in the current group
+        group_target = damage_counts[0]
+        # Start a count of how many we've seen in the current group
+        current_group = 0
+        current_group_start = -1
+        if sequence[i] == '.':
+            # This does not count towards anything
+            # Just advance to the next character
+            i += 1
+            continue
+        if sequence[i] == '?':
+            # We have some freedom to choose what will happen here
+            # If there is nothing in this group so far, then assume this is
+            # damaged
+            if not current_group:
+                # TODO: I believe this condition is always true
+                current_group = 1
+                current_group_start = i
+                # Now we need to be able to expand this group until we hit the
+                # target
+                while current_group < group_target:
+                    i += 1
+                    if i == len(sequence):
+                        # We've overshot the sequence length and failed
+                        # This does not contribute to the count
+                        # Reset and move on to the next character, ie., assume
+                        # that the group start was an operational spring
+                        # instead
+                        i = current_group_start + 1
+                        # Need to continue in the outer loop
+                        # So break outof this inner loop
+                        break
+                    if sequence[i] == '.':
+                        # Then we cannot expand the group
+                        # We have failed: this arrangement does not work
+                        # Reset and move on to the next character
+                        i = current_group_start + 1
+                        # Need to continue in the outer loop
+                        # So break out of this inner loop
+                        break
+                    current_group += 1
+                else:
+                    # Now that we're here, we've been able to expand the group
+                    # by the right amount
+                    # Now we need to make sure it is separated from all the
+                    # other groups
+                    i += 1
+                    if i < len(sequence) and sequence[i] == '#':
+                        # We've failed again: this arrangement does not work
+                        # Reset and move on to the next character
+                        i = current_group_start + 1
+                        continue  # this resets everything else
+                    # If we've made it here then things seem to have worked so
+                    # far
+                    if len(damage_counts) == 1:
+                        # This was the last group we needed to satisfy and
+                        # we've made it to the end
+                        # Now we just need to make sure that there are no more
+                        # broken springs in the rest of the string and, if
+                        # there aren't, then we can count this as a possible
+                        # arrangement
+                        if '#' in sequence[i+1:]:
+                            # We've failed
+                            i = current_group_start + 1
+                        else:
+                            # Count this as a possible arrangement
+                            #TODO: Can this be changed to return 1?
+                            counts += 1
+                            # Now reset everything and check if the starting
+                            # question mark could have been a good spring
+                            i = current_group_start + 1
+                            continue  # this resets everything else
+                    else:
+                        # Now we can recursively compute arrangements for the rest
+                        # of the sequence
+                        counts += count_permutations(
+                                sequence[i+1:],
+                                damage_counts[1:],
+                                )
+                        # Once we get to this point, we can go back to the start of
+                        # the group and go down the other path - assume that it was
+                        # an operational spring and look at what happens then
+                        # To do this, we just need to reset a few counters
+                        i = current_group_start + 1
+                        continue  # this resets everything else
+        elif sequence[i] == '#':
+            # We don't have the freedom to choose what to do here
+            # Start a new group here
+            current_group = 1
+            current_group_start = i
+            # Expand the group until we hit the target
+            while current_group < group_target:
+                i += 1
+                if i == len(sequence):
+                    # We've overshot the sequence length and failed
+                    return counts
+                if sequence[i] == '.':
+                    # Then we cannot expand the group
+                    # We have failed: this arrangement does not work
+                    return counts
+                current_group += 1
+            # Now that we're here, we've been able to expand the group by
+            # the right amount
+            # Now we need to make sure it is separated from all the other
+            # groups
+            i += 1
+            if i < len(sequence) and sequence[i] == '#':
+                # We've failed again: this arrangement does not work
+                return counts
+            if len(damage_counts) == 1:
+                # This was the last group we needed to satisfy and we've
+                # made it to the end
+                # Now we just need to make sure that there are no more
+                # broken springs in the rest of the string and, if
+                # there aren't, then we can count this as a possible
+                # arrangement
+                if '#' in sequence[i+1:]:
+                    # We've failed
+                    return counts
+                else:
+                    # Count this as a possible arrangement
+                    #TODO: Can this be changed to return 1?
+                    counts += 1
+                    return counts
+            else:
+                # Now we can recursively compute arrangements for the rest
+                # of the sequence
+                counts += count_permutations(
+                        sequence[i+1:],
+                        damage_counts[1:],
+                        )
+                # Once we get to this point, we can't go back to the start of
+                # the group and assume that the operational spring we started
+                # with is damaged
+                # So return the counts here
+                return counts
+    return counts
+
+def count_permutations_binary_based(sequence, damage_counts):
     """Count the permutations for a sequence
 
     This function takes in a sequence string and the counts of damaged springs
@@ -53,7 +200,6 @@ def count_permutations(sequence, damage_counts):
     # Now figure out how many more #'s we need
     num_needed_damages = sum(damage_counts) - num_present_damages
     # Now check if every variation of the ?'s yields a valid sequence
-    # for sequence_addition in it.chain.from_iterable(it.combinations(question_values, r) for r in range(len(question_values)+1)):
     for sequence_addition in it.combinations(question_values, num_needed_damages):
         test_sequence = base_sequence_value + sum(sequence_addition)
         # Convert this back to its string representation
@@ -76,8 +222,8 @@ def total_permutations(sequences, damage_counts):
     returns the total number of arrangements across all the different records.
     """
     num_permutations = 0
-    for sequence, damage_counts in zip(sequences, damage_counts):
-        num_permutations += (count := count_permutations(sequence, damage_counts))
+    for sequence, counts in zip(sequences, damage_counts):
+        num_permutations += count_permutations(sequence, counts)
     return num_permutations
 
 def parse_data(data):
@@ -106,4 +252,12 @@ def main(filename):
 
 if __name__ == "__main__":
     filename = sys.argv[1]
+    method = sys.argv[2]
+    match method:
+        case 'binary':
+            count_permutations = count_permutations_binary_based
+        case 'string':
+            count_permutations = count_permutations_string_based
+        case default:
+            raise ValueError(f'Unknown method "{method}"')
     sequences, damage_counts = main(filename)
